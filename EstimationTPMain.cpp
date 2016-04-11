c12a5e4a7a556dc39d25a41f7a29a5f99949c7a8
Clancy Emanuel <clancy.emanuel@uconn.edu>
2016-04-10 20:24:32 -0400

    about to pass a system matrix generator lambda instead of a hard coded matrix

--- EstimationTPMain.cpp
+++ EstimationTPMain.cpp
@@ -6,14 +6,21 @@ int main() {
   string filename = "/home/clancy/Projects/Estimation Project 2016/test.txt";
   cout << "Generating data in file " << filename<<endl;
   StateVector initial;
-  initial <<1,5,3;
+  initial <<1,1,1;
   SystemMatrix F;
+  F<< 2,0,0,
+      0,2,0,
+      0,0,2;
   pair<dataType ,dataType > interval(0.0,500);
   dataType samplingTime = 10;
   vector<pair<dataType ,dataType >> turnRates;
-  //turnRates.push_back(pair<dataType,dataType>())
+  turnRates.push_back(make_pair(100, 2));
+  turnRates.push_back(make_pair(130, 0));
+  turnRates.push_back(make_pair(200, -1));
+  turnRates.push_back(make_pair(245, 1));
+  turnRates.push_back(make_pair(335, -1));
+  turnRates.push_back(make_pair(380, 0));
   generateData(initial, F, interval, samplingTime, turnRates,filename);
-
 
   return 0;
 }
--- include/EstimationTPSimulator.h
+++ include/EstimationTPSimulator.h
@@ -10,18 +10,27 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 template<typename T, typename StateVector, typename SystemMatrix>
-void generateData(StateVector initial, SystemMatrix F, pair<T,T> timeInterval, T Ts, vector<pair<T,T>> turnRates, string filename) {
+void generateData(StateVector initial, function<SystemMatrix(T)> FGenerator, pair<T,T> timeInterval, T Ts, vector<pair<T,T>> turnRates, string filename) {
   ofstream outputFile;
+  auto lengthOfTest = timeInterval.second - timeInterval.first;
+  int numberOfDataPoints = floor(lengthOfTest / Ts);
+  vector<StateVector> data(numberOfDataPoints);
+  data[0] = initial;
   outputFile.open(filename);
-  cout<<"test = "<<initial<<endl;
-  outputFile<<initial(0)<<","<<initial(1)<<","<<initial(2)<<endl;
-  outputFile<<initial(0)<<","<<initial(1)<<","<<initial(2)<<endl;
+  auto it = data.begin();
+  it++;
+  for(it; it!= data.end(); it++) {
+    auto vec = *--it;//pre-decrement is essential
+    it++;//set the iterator back to its original value;
+    *it = (F * vec);//advance the system
+    outputFile<<vec(0)<<","<<vec(1)<<","<<vec(2)<<endl;//put the data in the file
+  }
   outputFile.close();
-
 };
 
 #endif //ESTIMATION_PROJECT_2016_ESTIMATIONTPSIMULATOR_H
