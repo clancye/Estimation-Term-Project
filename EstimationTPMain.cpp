748aedd999a90d1a6f4664aaf5ebb01c7080061d
Clancy Emanuel <clancy.emanuel@uconn.edu>
2016-04-22 00:25:41 -0400

    First test of measuring the target seems successful

--- EstimationTPMain.cpp
+++ EstimationTPMain.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+list<function<double(StateVector,StateVector)>> getMeasurementFunctions() {
+  list<function<double(StateVector,StateVector)>> functions;
+  random_device random_device1,random_device2;
+  mt19937 rangeNoiseGenerator(random_device1()), angleNoiseGenerator(random_device2());
+  normal_distribution<double> rangeNoise(0,50),angleNoise(0,.01745);//page 488, angle in rads
+  auto Range = function<double(StateVector,StateVector)> (
+          [rangeNoise,rangeNoiseGenerator]
+                  (StateVector sensorState, StateVector targetState) mutable {
+            double range = 0;
+            int size = sensorState.size();
+            for(int i = 0; i<size;i++) {
+              range += pow(sensorState(i)-targetState(i),2);
+            }
+            range = sqrt(range);
+            range += rangeNoise(rangeNoiseGenerator);
+            return range;
+          });
+  auto Azimuth = function<double(StateVector,StateVector)> (
+          [angleNoise,angleNoiseGenerator]
+                  (StateVector sensorState, StateVector targetState) mutable {
+            double x0 = sensorState(0), x1 = targetState(0), y0  = sensorState(2), y1 = targetState(2),azimuth;
+            azimuth = atan2(y1-y0,x1-x0);
+            azimuth += angleNoise(angleNoiseGenerator);
+            return azimuth;
+          });
+  functions.push_back(Range);
+  functions.push_back(Azimuth);
+  return functions;
+}
+
 int main() {
   string filename = "/home/clancy/Projects/Estimation Project 2016/NEWTEST.txt";
  /* cout << "Generating data in file " << filename<<endl;
@@ -10,34 +40,24 @@ int main() {
 
 
   DataType Ts = 1;
-  Target myTarget("My Target",filename);
-  myTarget.Advance(6);
-  StateVector x = myTarget.Sample();
+  Target target("My Target",filename);
 
-  random_device random_device1,random_device2;
-  mt19937 distanceNoiseGenerator(random_device1()), angleNoiseGenerator(random_device2());
-  normal_distribution<double> distanceNoise(0,50),angleNoise(0,.01745);//page 488, angle in rads
-  auto Distance = function<double(StateVector,StateVector)> (
-          [distanceNoise,distanceNoiseGenerator]
-          (StateVector sensorState, StateVector targetState) mutable {
-            double x0 = sensorState(0), x1 = targetState(0), y0  = sensorState(2), y1 = targetState(2),distance;
-            distance = sqrt(pow(x0-x1,2)+pow(y0-y1,2));
-            distance += distanceNoise(distanceNoiseGenerator);
-            return distance;
-  });
-  auto Azimuth = function<double(StateVector,StateVector)> (
-          [angleNoise,angleNoiseGenerator]
-          (StateVector sensorState, StateVector targetState) mutable {
-            double x0 = sensorState(0), x1 = targetState(0), y0  = sensorState(2), y1 = targetState(2),azimuth;
-            azimuth = atan2(y1-y0,x1-x0);
-            azimuth += angleNoise(angleNoiseGenerator);
-            return azimuth;
-  });
-  StateVector targetVec, sensorVec;
-  sensorVec<<1,0,0,0,0;
-  targetVec<<2,0,0,0,0;
-  cout<<Azimuth(sensorVec,targetVec)<<endl;
-  cout<<Azimuth(sensorVec,targetVec);
+  StateVector sensorState;
+  sensorState << -10000,0,0,0,0;
+
+  auto measurementFunctions = getMeasurementFunctions();
+
+  EstimationTPSensor sensor(sensorState,measurementFunctions);
+  auto m1 = sensor.Measure(target);
+  for(auto x:m1)
+    cout<<x<<",";
+  cout<<endl;
+  target.Advance(160);
+  m1 = sensor.Measure(target);
+  for(auto x:m1)
+    cout<<x<<",";
+  cout<<endl;
+
 
   /* Kalman Filter Stuff
   function<SystemMatrix()> _systemMatrixGenerator = [=]() {
