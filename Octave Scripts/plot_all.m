e7d8347a75b78d61a6f666c2e1db7fda779d0a61
Clancy Emanuel <clancy.emanuel@uconn.edu>
2016-04-29 01:56:27 -0400

    plot_all is shifting the measurements

--- Octave Scripts/plot_all.m
+++ Octave Scripts/plot_all.m
@@ -1,9 +1,10 @@
 clf
-immCTData = load('immCT.txt');
-immLData = load('immL.txt');
-kfData = load('kf.txt');
-tpData = load('Term Project Data.txt');
-measurements = load('measurements.txt');
+rootPath = "/home/clancy/Projects/Estimation Project 2016/Testing Data/";
+immCTData = load(strcat(rootPath,'immCT.txt'));
+immLData = load(strcat(rootPath,'immL.txt'));
+kfData = load(strcat(rootPath,'kf.txt'));
+tpData = load(strcat(rootPath,'Generated Target Trajectories/','Term Project Data.txt'));
+measurements = load(strcat(rootPath,'measurements.txt'));
 plot(tpData(:,1),tpData(:,3),"-b","linewidth",2);
 hold on
 plot(immCTData(:,1),immCTData(:,3),"-r","linewidth",2);
@@ -12,5 +13,5 @@ plot(immLData(:,1),immLData(:,3),"-g","linewidth",2);
 hold on
 plot(kfData(:,1),kfData(:,3),"c","linewidth",2);
 hold on
-plot(measurements(:,1),measurements(:,2),"-ks","linewidth",2);
+plot(measurements(:,1),measurements(:,2),"-k+","linewidth",2);
 
